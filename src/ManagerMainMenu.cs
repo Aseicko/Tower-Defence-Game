6fcd2331476446e866fc076a2e74a0b944be321d
Aseicko <Aseicko@ASEICKO-PC>
2024-11-17 16:45:10 +0700

    Implemented Main Menu display and input handler

---
+++ src/ManagerMainMenu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Tower_Defence_Game.src
+{
+    public interface IMainMenu
+    {
+        void Execute();
+    }
+
+    internal class ManagerMainMenu : IMainMenu
+    {
+        ConsoleKeyInfo inputMainMenu;
+
+        internal ManagerMainMenu()
+        {
+            Console.CursorVisible = false;
+
+        }
+
+        void IMainMenu.Execute()
+        {
+            this.Display();
+            this.Process();
+        }
+
+        private void Display()
+        {
+            Console.SetCursorPosition(0, 0);
+            Console.WriteLine
+            (
+                "<Main-Menu>\n" +
+                "001. Start\n" +
+                "002. Settings\n" +
+                "Esc. Exit\n" +
+                "</Main-Menu>"
+            );
+
+        }
+
+        private void Process()
+        {
+            inputMainMenu = Console.ReadKey(intercept: true);
+
+            switch (inputMainMenu.Key)
+            {
+                default:
+                    break;
+
+                case ConsoleKey.D1:
+                    this.EventStartGame();
+                    break;
+
+                case ConsoleKey.D2:
+                    this.EventOpenSettings();
+                    break;
+
+                case ConsoleKey.Escape:
+                    this.EventEndGame();
+                    break;
+            }
+
+        }
+
+        private void EventStartGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EventOpenSettings()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EventEndGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Thanks for playing!");
+            Environment.Exit(0);
+        }
+
+    }
+
+}
---
+++ src/Projects.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Tower_Defence_Game.src;
+
+internal class Projects
+{
+    static void Main()
+    {
+        IMainMenu mainMenu = new ManagerMainMenu(); ;
+
+        while (true)
+        {
+            mainMenu.Execute();
+
+        }
+
+    }
+
+}

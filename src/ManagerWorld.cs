a23ea379e486616dd0711fb412ec8b345e3392bc
Aseicko <Aseicko@ASEICKO-PC>
2024-11-17 17:51:58 +0700

    Implemented ManagerWorld Inicialize method

---
+++ src/ManagerWorld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Tower_Defence_Game.src
+{
+    internal class ManagerWorld
+    {
+        protected WorldTile[,] worldTileData;
+
+        protected (int, int) currentPlayerPosition = (0, 0);
+
+        internal ManagerWorld(uint sizeHorizontal, uint sizeVertical)
+        {
+            worldTileData = new WorldTile[sizeHorizontal, sizeVertical];
+
+            this.Inicialize();
+        }
+
+        private void Inicialize()
+        {
+            for (int i = 0; i < worldTileData.GetLength(0); i++)
+            {
+                for (int j = 0; j < worldTileData.GetLength(1); j++)
+                {
+                    worldTileData[i, j] = new WorldTile();
+                }
+            }
+
+        }
+
+    }
+
+}
